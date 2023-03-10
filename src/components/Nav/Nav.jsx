bcf8815edf34cb40b7eddb6129d938fa88ea7976
Nicolas Pua <nicopua7@gmail.com>
2023-03-10 00:14:44 -0300

    Componente Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,16 @@
 import SearchBar from '../Search/SearchBar.jsx'
+import {Link} from 'react-router-dom'
 
 export default function Nav({onSearch}) {
    return (
     <div>
+        <Link to='/home'>
+            <p>Home</p>
+        </Link>
+            
+        <Link to='/about'>
+            <p>About</p>
+        </Link>
         <SearchBar onSearch={onSearch}/>
     </div>
    );
