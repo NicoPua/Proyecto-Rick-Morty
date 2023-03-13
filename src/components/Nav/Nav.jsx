d6b5b750fdabc099e80c5229d048eb6da807c32d
Nicolas Pua <nicopua7@gmail.com>
2023-03-13 10:44:19 -0300

    Logout

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,16 +2,24 @@ import SearchBar from '../Search/SearchBar.jsx'
 import {Link} from 'react-router-dom'
 
 export default function Nav({onSearch}) {
-   return (
+    
+  const logout = () =>{
+
+  }
+  return (
     <div>
-        <SearchBar onSearch={onSearch}/>
-        <Link to='/home'>
-            <p>Home</p>
-        </Link>
-            
-        <Link to='/about'>
-            <p>About</p>
-        </Link>
+      <SearchBar onSearch={onSearch}/>
+      <Link to='/home'>
+        <p>Home</p>
+      </Link>
+        
+      <Link to='/about'>
+        <p>About</p>
+      </Link>
+
+      <Link to={logout}>
+        <p>Log Out</p>
+      </Link>
     </div>
-   );
+    );
 }
