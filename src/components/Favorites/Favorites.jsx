47bab563d730ab682ba9ab0ce82ef58394d7ba25
Nicolas Pua <nicopua7@gmail.com>
2023-03-16 19:20:19 -0300

    options, filtro y order

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,10 +1,33 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { filterCards, orderCards } from "../../redux/actions.js";
 import Card from "../Card/Card.jsx";
 
 const Favorites = () => {
     const favorites = useSelector((state)=> state.myFavorites)
+    const dispatch = useDispatch();
+
+    const ordenamiento = (event) =>{
+        dispatch(orderCards(event.target.value));
+    }
+    
+    const filtro = (event) =>{
+        dispatch(filterCards(event.target.value));
+    }
+
     return(
-    <>
+    <div>
+        <select name="Order" onChange={ordenamiento}>
+            <option value="Ascendente">Ascendente</option>
+
+            <option value="Descendente">Descendente</option>
+        </select>
+        <select name='Filter' onChange={filtro}>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Genderless">Genderless</option>
+            <option value="unknown">Unknown</option>
+        </select>
+
         {favorites.map(({id,name,species,gender,image})=>{
             return (
                 <Card 
@@ -17,7 +40,7 @@ const Favorites = () => {
                 />
             )
         })}
-    </>
+    </div>
     )
 }
 
