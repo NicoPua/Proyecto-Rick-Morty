00e7fceb5d1c9ab02953093faca04f299eac1a95
Nicolas Pua <nicopua7@gmail.com>
2023-03-16 19:20:53 -0300

    Action creators filter y order

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,22 @@
+//Action types
 export const ADD_CHAR = 'ADD';
 export const DEL_CHAR = 'DELETE';
+export const FILTER = 'FILTER';
+export const ORDER = 'ORDER';
 
+//Action creators
 export const addCharacters = (character) =>{
     return { type: ADD_CHAR, payload: character }
 }
 
 export const delCharacters = (id) =>{
     return { type: DEL_CHAR, payload: id }
+}
+
+export const filterCards = (gender) => {
+    return { type: FILTER, payload: gender }
+}
+
+export const orderCards = (id) => {
+    return { type: ORDER, payload: id }
 }
