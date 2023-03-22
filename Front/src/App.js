11bebc175f65cb401cce60c4a5e8e47550997aa1
Nicolas Pua <nicopua7@gmail.com>
2023-03-22 16:48:13 -0300

    asd

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,46 +1,52 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
-import Cards from './components/Cards/Cards.jsx'
-import Nav from './components/Nav/Nav.jsx'
-import About from './views/About.jsx'
-import Detail from './views/Detail.jsx'
-import Form from './components/Form/Form.jsx'
-import Favorites from './components/Favorites/Favorites'
+import Cards from './components/Cards/Cards.jsx';
+import Nav from './components/Nav/Nav.jsx';
+import About from './views/About.jsx';
+import Detail from './views/Detail.jsx';
+import Form from './components/Form/Form.jsx';
+import Favorites from './components/Favorites/Favorites';
 
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 
 
 function App () {
   const [characters,setCharacters] = useState([]);
 
+  //FUNCION PARA BUSCAR----------------------------------------------
   const onSearch = (id) => {
     
     const URL = 'http://localhost:3001/rickandmorty';
+
+
     //const URL = 'https://be-a-rym.up.railway.app/api';
-    //const KEY = 'ab609bfc7704.d3a36031c2ddc6820402';  ?key=${KEY}
+    //const KEY = 'ab609bfc7704.d3a36031c2ddc6820402';  
+    //`${URL}/character/${id}?key=${KEY}`
 
     if (characters.find((char) => char.id == id)) {
-      return alert('Personaje Repetido');
+      return alert('El Personaje ya fue añadido');
     }
 
-    fetch(`${URL}/characters/${id}`)
+    fetch(`${URL}/character/${id}`)
       .then((response) => response.json())
       .then((data) => {
-      if (data.name) {
-        setCharacters((oldChar) => [...oldChar, data]); //Recibo el estado viejo y retorno el estado nuevo.
-      } else {
-        alert('No hay personajes con ese ID');
-      }
+        console.log(data);
+        if (data.name) {
+          setCharacters((oldChar) => [...oldChar, data]); //Recibo el estado viejo y retorno el estado nuevo.
+        } else {
+          alert('No hay personajes con ese ID');
+        }
     });  
   }
+  //----------------------------------------------------------------------------
 
-  //LOGIN
+  //LOGIN ----------------------------------------------------------------------
   const [access, setAccess] = useState(false);
   const username = 'nicopua7@gmail.com';
   const password = 'asd1234';
+  
   const navigate = useNavigate();
-
   const login = (userData) => {
     if (userData.username == username && userData.password == password) {
       setAccess(true);
@@ -53,9 +59,11 @@ function App () {
   }, [access]);
   //------------------------------------------------------------------------
   
+  //FUNCION CerrarCARD--------------------------------------------------------------
   const onClose = (id) => {
     setCharacters(characters.filter((char) => char.id !== id));
   }
+  //------------------------------------------------------------------------
 
   const location = useLocation(); 
  
