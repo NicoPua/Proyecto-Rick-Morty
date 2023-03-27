e7444f0c14f5e6cc43a6ba7c778c7d8b009dbce3
Nicolas Pua <nicopua7@gmail.com>
2023-03-26 22:08:02 -0300

    a

---
+++ Front/src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const AllBar = styled.div`
+    background-color: black;
+    width: 100%;
+    display: flex;
+
+    jusfity-content: space-between;
+`
+
+export const LinksBar = styled.div`
+    width: 75%;    
+    display:flex;
+    
+    justify-content: space-around;
+`
