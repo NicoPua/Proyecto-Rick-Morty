ad820e8208b710f948b20880c0b4f41358772ff8
Nicolas Pua <nicopua7@gmail.com>
2023-03-31 17:07:27 -0300

    server

--- Back/src/server.js
+++ Back/src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const router = require("./routes/index.js");
+const cors = require("cors");
+
 const server = express();
 const PORT = 3001;
 
-const router = require("./routes/index.js")
-
+server.use(cors());
 server.use(express.json());
 server.use("/", router);
 
