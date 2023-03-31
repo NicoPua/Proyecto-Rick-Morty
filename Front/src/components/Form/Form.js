a3da13a43d3ddc8e8d8a9756d98640dd66a74d40
Nicolas Pua <nicopua7@gmail.com>
2023-03-31 17:08:48 -0300

    diseno form

---
+++ Front/src/components/Form/Form.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+
+export const ContainerGlobal = styled.div`
+    height: 550px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+
+    
+`
+export const Inicio = styled.div`
+    background-color: #994C00;
+    width: 60%;
+    height: 400px;
+    border-radius: 20px;
+    box-shadow: -8px 9px 14px -3px rgba(0,0,0,0.75);
+    
+    margin-top: 75px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    
+` 
+
+export const Titulos = styled.div`
+    font-family: sans-serif;
+    font-style: oblique;
+    text-decoration: underline;
+` 
+
+export const Info = styled.div`
+    background-color: white;
+    height: 400px;
+    width: 300px;
+    border-radius: 20px;
+    box-shadow: -8px 9px 14px -3px rgba(0,0,0,0.75);
+
+    padding-top: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
+    margin-top 75px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    
+` 
+
+export const Publi = styled.div`
+    
+    height: 70px;
+    display:flex;
+    flex-direction: row;
+    justify-content: space-around;
+    
+` 
+
+export const ImgHenry = styled.img`
+    width: 40px;
+    height: 40px;
+    box-shadow: -8px 9px 14px -3px rgba(0,0,0,0.75);
+    border-radius: 20px;
+`
