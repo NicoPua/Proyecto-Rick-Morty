48d07f9aeee245732eee6d2b2069b3f85961a011
Nicolas Pua <nicopua7@gmail.com>
2023-04-04 23:48:19 -0300

    CSS About.js

---
+++ Front/src/views/About.js
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+export const ContainerGlobal = styled.div`
+    background-color: white;
+    height: 570px;
+
+    display: flex;
+    justify-content: space-around;
+`
+
+export const InfoRyM = styled.div`
+    background-color: brown;
+    height: 570px;
+    width: 70%;
+
+
+`
+export const InfoAboutMe = styled.div`
+    background-color: violet;
+    height: 570px;
+    width: 25%;
+
+
+`
+
+export const LinksImgs = styled.img`
+    width: 15%;
+`
