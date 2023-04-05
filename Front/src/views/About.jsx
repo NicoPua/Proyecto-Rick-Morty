c3fe367ece2128027944ced1ee5fb382ef9f4b7b
Nicolas Pua <nicopua7@gmail.com>
2023-04-05 19:52:00 -0300

    About CSS

--- Front/src/views/About.jsx
+++ Front/src/views/About.jsx
@@ -1,4 +1,4 @@
-import {ContainerGlobal, InfoRyM, InfoAboutMe,LinksImgs} from "./About"
+import {ContainerGlobal, InfoRyM,       InfoAboutMe,ImgProfile,ContImgs,LinksImgs} from "./About"
 
 const About = () =>{
     return(
@@ -6,34 +6,47 @@ const About = () =>{
             <InfoRyM>
                 <h2>Rick and Morty API</h2>
                 
-                <p>Página creada y diseñada desde el Back-End hasta el Front-end de la mano de Henry.</p>
-                <p></p>
+                <p>-Página creada y diseñada desde el Back-End hasta el Front-end de la mano de Henry.-</p>
+                <h3>Descripción</h3>
+                <p>asdsadsadsdadasfaa</p>
                 
             </InfoRyM>
 
             <InfoAboutMe>
                 <h3>My Profile</h3>
+                <ImgProfile src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRghxLUSuc5cd4DYd_vqSF4HF4bGT2GMCRgzX408QhB_dFRInZ5Cvy3rikPAwXOtEVesnQ&usqp=CAU"/>
                 <ul>
                     <li>Name: Gonzalo Nicolás Púa</li>
                     <li>Country: Buenos Aires, Argentina</li>
                     <li>Graduate: Technical School n°1 "Otto Krause"</li>
                     <li>Title: Electronic Technician</li>
                 </ul>
-                <br/>
+                
                 <h3>Contacts</h3>
                 <ul>
                     <li>E-mail: nicopua7@gmail.com</li>
                     <li>LinkedIn: Gonzalo Nicolas Pua</li>
                     <li>GitHub: NicoPua</li>
                 </ul>
-                <br/>
+                
                 <h3>Links</h3>
-                <div>
-                    <LinksImgs src="https://startupeable.com/directorio/wp-content/uploads/2021/03/d4face92a7abc37a414e0bc3acf4ff23ec588438.png"/>
-                    <LinksImgs src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/>
-                    <LinksImgs src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png"/>
-                    <LinksImgs src="https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon-thumbnail.png"/>
-                </div>
+                <ContImgs>
+                    <a target="_blank" rel="noopener noreferrer" href='https://www.soyhenry.com/'>
+                        <LinksImgs src="https://startupeable.com/directorio/wp-content/uploads/2021/03/d4face92a7abc37a414e0bc3acf4ff23ec588438.png"/>
+                    </a>
+
+                    <a target="_blank" rel="noopener noreferrer" href='https://github.com/NicoPua'>
+                        <LinksImgs src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/>
+                    </a>
+
+                    <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/gonzalo-nicolas-pua-a962901b6/'>
+                        <LinksImgs src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png"/>
+                    </a>
+
+                    <a target="_blank" rel="noopener noreferrer" href='https://es.reactjs.org/'>
+                        <LinksImgs src="http://ibthemespro.com/docs/beny/img/side-nav/cmm4.png"/>
+                    </a>
+                </ContImgs>
             </InfoAboutMe>
         </ContainerGlobal>
     )
