bb4626db76c66ea9f620b5bb2a82ce67274c6022
Nicolas Pua <nicopua7@gmail.com>
2023-04-07 17:50:37 -0300

    Routes Update

--- Back/src/routes/index.js
+++ Back/src/routes/index.js
@@ -1,14 +1,14 @@
 const {Router} = require("express")
 const getCharById = require("../controllers/getCharById.js")
 const getCharDetail = require("../controllers/getCharDetail.js")
-//const getCharFav =
+
 let favs = require("../utils/favs.js");
 
 const router = Router();
-
-router.get("/onsearch/:id", getCharById);               //PIDO INFORMACION resumida de un personaje segun ID.
-
-router.get("/detail/:id", getCharDetail);               //PIDO INFORMACION detallada de un personaje segun ID.
+    
+router.get("/onsearch/:id", getCharById);   //FALTA APLICAR ASYNC AWAIT
+         
+router.get("/detail/:id", getCharDetail);   //FALTA APLICAR ASYNC AWAIT            
 
 router.post("/rickandmorty/fav", (req,res) => {         //GUARDO INFORMACION de un FAV
     favs.push(req.body);
@@ -21,10 +21,8 @@ router.get("/rickandmorty/fav", (req,res) => {          //PIDO INFORMACION de la
 
 router.delete("/rickandmorty/fav/:id", (req,res) => {   //BORRA INFORMACION segun el ID
     const {id} = req.params;
-    if(!id) res.status(402).send({"error": "Falta ingresar la ID"});
-
-    const newFavs = favs.filter((fav) => fav.id !== id);
-    res.status(200).json(newFavs);
+    favs = favs.filter((fav) => fav.id !== id);
+    res.status(200).json({status: "ok"});
 });
 
 module.exports = router;
