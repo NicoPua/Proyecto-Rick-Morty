3b0b1d5c91edae6295f7677b858dd375f2abb1ff
Nicolas Pua <nicopua7@gmail.com>
2023-04-07 17:51:54 -0300

    getCharDetail update

--- Back/src/controllers/getCharDetail.js
+++ Back/src/controllers/getCharDetail.js
@@ -1,17 +1,16 @@
 const axios = require("axios");
+const URL = 'https://be-a-rym.up.railway.app/api';
+const KEY = 'ab609bfc7704.d3a36031c2ddc6820402'; 
 
-const getCharDetail =(req,res) => {
-    const URL = 'https://be-a-rym.up.railway.app/api';
-    const KEY = 'ab609bfc7704.d3a36031c2ddc6820402';    
+const getCharDetail = (req,res) => { //PIDO INFORMACION detallada de un personaje segun ID.
     const {id} = req.params;
-
     axios.get(`${URL}/character/${id}?key=${KEY}`)
     .then((response) => {
-        const {id, name, species, image, gender,origin} = response.data;
-        res.status(200).json({id, name, species, image, gender,origin});
+        const {id, name, status ,species, image, gender,origin} = response.data;
+        res.status(200).json({id, name, status , species, image, gender,origin});
     })
     .catch((error) => {
-        res.status(500).json(error.message);
+        res.status(500).json({"error": error.message});
     }) 
 }
 
