f1b888e2f415b0d7640fad242eeb28424addf12f
Nicolas Pua <nicopua7@gmail.com>
2023-04-07 17:53:10 -0300

    Actions Update

--- Front/src/redux/actions.js
+++ Front/src/redux/actions.js
@@ -1,53 +1,49 @@
 import axios from 'axios';
 
 //Action types
-export const ADD_CHAR = 'ADD';
-export const DEL_CHAR = 'DELETE';
-export const FILTER = 'FILTER';
-export const ORDER = 'ORDER';
-
+export const ADD_CHAR = 'ADD_CHARACTER';
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
 export const GET_CHARACTER_DETAIL = 'GET_CHARACTER_DETAIL'
 export const GET_FAVORITES = "GET_FAVORITES";
 export const CLEAN_DETAIL = "CLEAN_DETAIL";
 
-//Action creators
-export const addCharacters = (character) =>{
-    return function (dispatch) {
-        axios.post("http://localhost:3001/rickandmorty/fav", character)
-        .then((response) => {
-            return dispatch({ type: ADD_CHAR, payload: character });
-        })  
-    }
-    //return { type: ADD_CHAR, payload: character }
-}
+export const FILTER_FAVORITES = 'FILTER_FAVORITES';
+export const ORDER_FAVORITES = 'ORDER_FAVORITES';
 
-export const delCharacters =  (id) =>{  
-    return async function  (dispatch) {
-        await axios.delete(`http://localhost:3001/rickandmorty/fav/${id}`)
-        dispatch({ type: DEL_CHAR, payload: id });
-        alert("Eliminado con éxito");
-    }  
-    //return { type: DEL_CHAR, payload: id }
-}
+//Action creators
+
+export const addFavorite = (character) =>{
+    return function() {
+        axios.post("http://localhost:3001/rickandmorty/fav", character)         //GUARDO la información de "character" en la URL/fav
+        .then((res) => console.log("Personaje Agregado"));
+    }
+ }
+
+export const removeFavorite = (id) =>{
+    return async function (dispatch) {
+        await axios.delete(`http://localhost:3001/rickandmorty/fav/${id}`)      //Pido BORRAR el character según la ID
+        dispatch(getFavorites());                                               //Actualizo los favoritos.
+        alert("Eliminado con éxito");                                           //Mando una alerta.
+    }
+                                             
+ }
 
 export const getFavorites = () => {
     return async function (dispatch) {
-        const URL = "http://localhost:3001";
-        const response = await axios.get(`${URL}/rickandmorty/fav`);
+        const response = await axios.get(`http://localhost:3001/rickandmorty/fav`);
         dispatch({ type: GET_FAVORITES, payload: response.data });
       };
 } 
 
 export const getCharacterDetail = (id) =>{
     return async function (dispatch) {
-        const URL = "http://localhost:3001";
-        const response = await axios.get(`${URL}/detail/${id}`);
+        const response = await axios.get(`http://localhost:3001/detail/${id}`);
         dispatch({ type: GET_CHARACTER_DETAIL, payload: response.data });
     };
 }
 
-export const filterCards = (gender) => { return { type: FILTER, payload: gender } }
+export const filterCards = (gender) => { return { type: FILTER_FAVORITES, payload: gender } }
 
-export const orderCards = (id) => { return { type: ORDER, payload: id } }
+export const orderCards = (id) => { return { type: ORDER_FAVORITES, payload: id } }
 
 export const cleanDetail = () => { return { type: CLEAN_DETAIL } };
